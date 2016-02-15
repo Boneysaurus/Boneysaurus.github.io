b39c12dad6d4d3a81310f7200d65a82d0ed7a43f
tCalc <isaboulle.gauthier+tcalc@gmail.com>
2016-02-16 02:12:17 +0800

    something new

--- script/main.js
+++ script/main.js
@@ -1,3 +1,11 @@
+var charClass = "";
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
+    $(".classB").click(function(){
+        charClass= $(this).text();
+        $("#classL").text(charClass);
+    })
+
 });
+
+
